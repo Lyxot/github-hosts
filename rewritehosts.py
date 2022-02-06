bc712349e8e50e90cb1e7dab7636cac2519c8b7c
A_jiu <34974508+A-JiuA@users.noreply.github.com>
2022-02-06 21:42:59 +0800

    Update rewritehosts.py

--- rewritehosts.py
+++ rewritehosts.py
@@ -1,90 +1,56 @@
 # -*- coding: UTF-8 -*-
-from requests import get,Session,exceptions
-from requests.adapters import HTTPAdapter
-from os import name
+import requests,os
 
-urls = ['github.com','github.global.ssl.fastly.net','codeload.github.com','raw.githubusercontent.com']
+urls = ['github.com','codeload.github.com','github.global.ssl.fastly.net','codeload.github.com','api.github.com','nodeload.github.com','raw.github.com','training.github.com','assets-cdn.github.com','documentcloud.github.com','help.github.com']
+githubusercontent_urls = ['raw.githubusercontent.com','pkg-containers.githubusercontent.com','cloud.githubusercontent.com','gist.githubusercontent.com','marketplace-screenshots.githubusercontent.com','repository-images.githubusercontent.com','user-images.githubusercontent.com','desktop.githubusercontent.com','avatars.githubusercontent.com','avatars0.githubusercontent.com','avatars1.githubusercontent.com','avatars2.githubusercontent.com','avatars3.githubusercontent.com','avatars4.githubusercontent.com','avatars5.githubusercontent.com','avatars6.githubusercontent.com','avatars7.githubusercontent.com','avatars8.githubusercontent.com']
 
 def get_ip(url):
     headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36 Edg/84.0.522.59'}
-    html = get(url,headers=headers)
-    html.encoding = 'utf-8'
-    html = html.text
-    ip = html[html.find('www.ipaddress.com/ipv4/') + 23:html.find('\\',html.find('www.ipaddress.com/ipv4/'))]
+    html = requests.get('https://ipaddress.com/website/'+url,headers=headers).text
+    ip = html[html.find('<ul class="comma-separated"><li>') + 32:]
+    ip = ip[:ip.find('</li>')]
     return ip
 
 def rewrite_hosts(result):
-    if name == 'nt':
-        try:
-            with open('C:\\Windows\\System32\\drivers\\etc\\hosts','r') as r:
-                lines=r.readlines()
-            with open('C:\\Windows\\System32\\drivers\\etc\\hosts','w') as w:
-                for l in lines:
-                    if urls[0] in l or urls[1] in l or urls[2] in l or urls[3] in l or l == '\n':
-                        continue
-                    w.write(l)
-            with open('C:\\Windows\\System32\\drivers\\etc\\hosts','a') as hosts:
-                hosts.write(result)
-            print('Success! 成功!\nPlease reset the HOSTS file if there are any exceptions below 如有以下异常请重置hosts文件\nSlow Loading github.com,Unable to connect github.com      Github访问慢,无法访问Github')
-        except IOError:
-            print('Fail,please try to run this script as an administrator 失败,请以管理员身份运行这个脚本')
-        except:
-            print('Unknown error,please create an issue 未知错误，请创建issue')
-    elif name == 'posix':
-        try:
-            with open('/etc/hosts','r') as r:
-                lines=r.readlines()
-            with open('/etc/hosts','w') as w:
-                for l in lines:
-                    if urls[0] in l or urls[1] in l or urls[2] in l or urls[3] in l or l == '\n':
-                        continue
-                    w.write(l)
-            with open('/etc/hosts','a') as hosts:
-                hosts.write(result)
-            print('Success! 成功!\nPlease reset the HOSTS file if there are any exceptions below 如有以下异常请重置hosts文件\nSlow Loading github.com,Unable to connect github.com      Github访问慢,无法访问Github')
-        except IOError:
-            print('Fail,please try to run this script as root 失败,请以root用户运行这个脚本')
-        except:
-            print('Unknown error,please create an issue 未知错误，请创建issue')
+    if os.name == 'nt':
+        path='C:\\Windows\\System32\\drivers\\etc\\hosts'
+    elif os.name == 'posix':
+        path='/etc/hosts'
+    with open(path,'r+') as f:
+        context=f.read()
+        for i in urls+githubusercontent_urls:
+            context=context.replace(context[context[:context.find(i)+len(i)].rfind('\n'):context.find(i)+len(i)],'')
+        context=context+'\n'+result
+        f.seek(0,0)
+        print(context)
+        f.write(context)
 
-try:
-    if __name__ == "__main__":
-        Session().mount('https://',HTTPAdapter(max_retries=3))
-        if name == 'nt':
-            print('System: Windows')
-            ip = get_ip('https://' + urls[0] + '.ipaddress.com')
-            result = ip + ' ' + urls[0]
-            print('url: ' + urls[0] + ' ip: ' + ip)
-            ip = get_ip('https://fastly.net.ipaddress.com/' + urls[1])
-            result = result + '\r\n' + ip + ' ' + urls[1]
-            print('url: ' + urls[1] + ' ip: ' + ip)
-            ip = get_ip('https://github.com.ipaddress.com/' + urls[2])
-            result = result + '\r\n' + ip + ' ' + urls[2]
-            print('url: ' + urls[2] + ' ip: ' + ip)
-            ip = get_ip('https://githubusercontent.com.ipaddress.com/' + urls[3])
-            result = result + '\r\n' + ip + ' ' + urls[3]
-            print('url: ' + urls[3] + ' ip: ' + ip)
-        elif name == 'posix':
-            print('System: Linux/Unix')
-            ip = get_ip('https://' + urls[0] + '.ipaddress.com')
-            result = ip + ' ' + urls[0]
-            print('url: ' + urls[0] + ' ip: ' + ip)
-            ip = get_ip('https://fastly.net.ipaddress.com/' + urls[1])
-            result = result + '\n' + ip + ' ' + urls[1]
-            print('url: ' + urls[1] + ' ip: ' + ip)
-            ip = get_ip('https://github.com.ipaddress.com/' + urls[2])
-            result = result + '\n' + ip + ' ' + urls[2]
-            print('url: ' + urls[2] + ' ip: ' + ip)
-            ip = get_ip('https://githubusercontent.com.ipaddress.com/' + urls[3])
-            result = result + '\n' + ip + ' ' + urls[3]
-            print('url: ' + urls[3] + ' ip: ' + ip)
-        else:
-            print('Unsupported system! 不支持的系统!')
-            exit()
+if __name__ == "__main__":
+    try:
+        result=''
+        for i in urls:
+            result=result+(get_ip(i))+' '+i+'\n'
+        result=result+'''185.199.108.133 raw.githubusercontent.com
+185.199.108.154 pkg-containers.githubusercontent.com
+185.199.108.133 cloud.githubusercontent.com
+185.199.108.133 gist.githubusercontent.com
+185.199.108.133 marketplace-screenshots.githubusercontent.com
+185.199.108.133 repository-images.githubusercontent.com
+185.199.108.133 user-images.githubusercontent.com
+185.199.108.133 desktop.githubusercontent.com
+185.199.108.133 avatars.githubusercontent.com
+185.199.108.133 avatars0.githubusercontent.com
+185.199.108.133 avatars1.githubusercontent.com
+185.199.108.133 avatars2.githubusercontent.com
+185.199.108.133 avatars3.githubusercontent.com
+185.199.108.133 avatars4.githubusercontent.com
+185.199.108.133 avatars5.githubusercontent.com
+185.199.108.133 avatars6.githubusercontent.com
+185.199.108.133 avatars7.githubusercontent.com
+185.199.108.133 avatars8.githubusercontent.com'''
         rewrite_hosts(result)
-except KeyboardInterrupt:
-    print('User termination 用户终止')
-except (exceptions.ConnectionError,exceptions.HTTPError,exceptions.UnrewindableBodyError,exceptions.RetryError,exceptions.SSLError,exceptions.ConnectTimeout,exceptions.Timeout,exceptions.URLRequired,exceptions.TooManyRedirects,exceptions.MissingSchema,exceptions.InvalidSchema,exceptions.InvalidURL,exceptions.InvalidHeader,exceptions.ChunkedEncodingError,exceptions.StreamConsumedError,exceptions.ContentDecodingError):
-    print('Fail,please check your network connection 失败,请检查你的网络连接')
-except:
-    print('Unknown error,please create an issue 未知错误，请创建issue')
+        print('Done!')
+    except IOError:
+        print('Please run as administrator\n请以管理员身份运行')
+    except:
+        print('Error')
